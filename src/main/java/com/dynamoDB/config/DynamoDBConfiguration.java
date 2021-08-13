e35cc424ff9cc2f33b491956e83ce7592fd3216d
tanmaypardeshi <tanmaypardeshi@gmail.com>
2021-08-13 13:13:29 +0530

    Completed basic CRUD using AWS DynamoDB

--- src/main/java/com/dynamoDB/config/DynamoDBConfiguration.java
+++ src/main/java/com/dynamoDB/config/DynamoDBConfiguration.java
@@ -6,10 +6,13 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DynamoDBConfiguration {
+
+    @Bean
     public DynamoDBMapper dynamoDBMapper() {
         return new DynamoDBMapper(buildAmazonDynamoDB());
     }
